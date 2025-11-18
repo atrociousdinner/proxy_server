639b9f1af7eec0fb600d17e9da54644a356faf9a
atrociousdinner <nerishcodes@gmail.com>
2025-11-18 21:01:14 +0545

    Simple commander implementation

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
+#!/usr/bin/env node
+
+
 import express from "express";
 import { createClient } from "redis";
 import dotenv from "dotenv";
+dotenv.config();
+import {program} from "commander";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+//Redis section
 const client = createClient({
-    url: process.env.REDIS_URL
-})
+  url: process.env.REDIS_URL,
+});
 
-client.on("error", (err) => console.error("Redis Client Error:", err))
+client.on("error", (err) => console.error("Redis Client Error:", err));
 
 await client.connect();
-console.log("Connected to Redis")
-    
+console.log("Connected to Redis");
 
+
+//Commander section
+program
+  .option("-p, --port <number>", "specify the port number of your cache server")
+program.parse()
+
+const options = program.opts()
+const PORT = options.port || 3000
+
+
+//Express section
 app.get("/", async (req, res) => {
-    await client.set("name", "Nerish")
-    const value = await client.get("name")
-    res.send(`Value from redis: ${value}`)
+  await client.set("name", "Nerish");
+  const value = await client.get("name");
+  res.send(`Value from redis: ${value}`);
 });
 
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
+
+
+
