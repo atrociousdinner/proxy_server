73dd4d35e6ed76fa0aacf680dc4252423fb1ee46
atrociousdinner <nerishcodes@gmail.com>
2025-11-19 12:51:16 +0545

    Direct to the server

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
 
-
 import express from "express";
 import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
-import {program} from "commander";
+import { program } from "commander";
+import axios from "axios";
 
+
+
+//Global variables
 const app = express();
+
+
+const fetchApiData = async (url, route) => {
+  try {
+    const full_url = url + route
+    console.log(full_url)
+    const response = await fetch(full_url)
+    const data = await response.json()
+    console.log(`Request sent to the API`)
+    console.log(data)
+  }
+  catch (err) {
+    console.error(`Cannot fetch API: `, err)
+  }
+}
 
 //Redis section
 const client = createClient({
@@ -23,21 +41,40 @@ console.log("Connected to Redis");
 //Commander section
 program
   .option("-p, --port <number>", "specify the port number of your cache server")
+  .option("-o, --origin <url>", "specify the server to request")
 program.parse()
 
 const options = program.opts()
 const PORT = options.port || 3000
+const server_url = options.origin
 
 
 //Express section
-app.get("/", async (req, res) => {
-  await client.set("name", "Nerish");
-  const value = await client.get("name");
-  res.send(`Value from redis: ${value}`);
+// app.use('*', (req, res, next) => {
+//   const endpoint = req.path;       // /api/products
+//   const fullUrl = req.originalUrl; // /api/products?limit=10
+//   const method = req.method;       // GET, POST, PUT, etc.
+
+//   console.log('Method:', method);
+//   console.log('Endpoint:', endpoint);
+//   console.log('Full URL:', fullUrl);
+
+//   next(); // continue to route handler
+// });
+
+
+app.use(async (req, res) => {
+  const route = req.originalUrl
+  const method = req.method
+  // const fullUrl = req.fullUrl
+
+  console.log('Method:', method);
+  // console.log('Full URL:', fullUrl);
+  await fetchApiData(server_url, route)
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on PORT: ${PORT}`);
+  console.log(`App listening on PORT: ${PORT}. Forward request to ${server_url}`);
 });
 
 
